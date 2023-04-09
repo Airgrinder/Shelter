ae81cac34f7e19e63d2100c7b410224421db9b67
Airgrinder <51jework@gmail.com>
2023-04-09 05:31:49 +0300

    feat: add side menu(09.04.2023 5:30)

---
+++ shelter/assets/scripts/burgerMenu.js
@@ -0,0 +1,29 @@
+let burger = document.getElementsByClassName("burger-menu")[0]
+let burgerBG = document.getElementsByClassName("header-mobile-menu-background")[0]
+let burgerMenu = document.getElementsByClassName("header-mobile-menu")[0]
+let links = document.getElementsByClassName("header-mobile-menu-link");
+let body = document.getElementsByTagName("body")[0]
+
+
+export function showSideMenu() {
+    if (burgerMenu.style.right === "0px") {
+        burgerMenu.style.right = "-100%"
+        burgerBG.style.right = "-100%"
+        burger.style.transform = "rotate(0deg)"
+        burger.style.margin = ""
+        body.style.overflow = ""
+    } else {
+        burgerMenu.style.right = "0px"
+        burgerBG.style.right = "0px"
+        burger.style.transform = "rotate(90deg)"
+        burger.style.margin = "30px 33px 0 0"
+        body.style.overflow = "hidden"
+    }
+}
+
+burgerBG.addEventListener('touchstart', showSideMenu);
+burger.addEventListener('touchstart', showSideMenu);
+for (const link of links) {
+    link.addEventListener('touchend', showSideMenu);
+}
+
--- shelter/main/script.js
+++ shelter/main/script.js
@@ -1,3 +1,8 @@
 window.addEventListener('load', function() {
     document.body.classList.remove('preload');
 });
+
+import {showSideMenu} from '../assets/scripts/burgerMenu.js'
+
+
+
